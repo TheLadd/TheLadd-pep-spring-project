57b7475f984a2ab1ae8f6304feb1a7e4b7f32677
Owen Ribera <owenribera@gmail.com>
2024-09-27 22:42:05 +0000

    Implemented stubs for registerUser(), loginUser(), and getAllMessages() in the Controller

--- src/main/java/com/example/controller/SocialMediaController.java
+++ src/main/java/com/example/controller/SocialMediaController.java
@@ -1,5 +1,10 @@
 package com.example.controller;
 
+import com.example.entity.Account;
+import com.example.entity.Message;
+
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
 /**
  * TODO: You will need to write your own endpoints and handlers for your controller using Spring. The endpoints you will need can be
@@ -7,6 +12,31 @@ package com.example.controller;
  * where applicable as well as the @ResponseBody and @PathVariable annotations. You should
  * refer to prior mini-project labs and lecture materials for guidance on how a controller may be built.
  */
+@RestController
 public class SocialMediaController {
+
+    // TODO
+    @PostMapping(value = "/register", params = {"username", "password"})
+    public Account registerUser(@RequestBody Account acc) {
+        return null;
+    } 
+
+    // TODO
+    @PostMapping(value = "/login", params = {"username", "password"})
+    public Account loginUser(@RequestBody Account acc) {
+        return null;
+    }
+
+    // TODO
+    @PostMapping(value = "/messages")
+    public Message createMessage(@RequestBody Message msg) {
+        return null;
+    }
+
+    // TODO
+    @GetMapping(value = "/messages")
+    public List<Message> getAllMessages() {
+        return null;
+    }
 
 }
